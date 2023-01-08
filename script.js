16f16e2f355e46de30b6d9961a4645eb3501a18b
jeanvand <114246668+jeanvand@users.noreply.github.com>
2023-01-08 21:08:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("tout fonctionne");
 
-const button = document.querySelector("button");
+const button = document.getElementById("button0");
 // const container = document.querySelector(".container");
 const cards = document.querySelectorAll('.cards');
 
@@ -29,3 +29,27 @@ button.addEventListener("click", function () {
     cards[i].style.transitionDelay = (0.40 * i) + "s";
     }
 });
+
+
+
+const button1 = document.getElementById("button1");
+const element = document.getElementById("card0");
+button1.addEventListener("click", function() {
+  element.classList.toggle("animated");
+});
+
+const button2 = document.getElementById("button2");
+const body = document.getElementById("body");
+button2.addEventListener("click", function() {
+  body.classList.toggle("violet");
+})
+
+const button3 = document.getElementById("button3");
+button3.addEventListener("click", function() {
+  body.classList.toggle("noir");
+})
+
+const button4 = document.getElementById("button4");
+button4.addEventListener("click", function() {
+  body.classList.toggle("rouge");
+})
